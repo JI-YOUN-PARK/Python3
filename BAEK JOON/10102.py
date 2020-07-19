e32c05db7e008adca8380b43a4ade89a9d59bbf4
JI-YOUN-PARK <pjiyun102@gmail.com>
2020-07-19 16:42:26 +0900

    *

--- BAEK JOON/10102.py
+++ BAEK JOON/10102.py
@@ -1,6 +1,7 @@
 v=int(input("심사위원의 수 : "))
 total = input("투표하세요! : ")
-#v를 입력받아 v개 만큼 수를 입력하고자 했는데, python에 그런 코드는 없다고 한다.
+#v를 입력받아 v개 만큼 수를 입력하고자 했는데, python에 그런 코드는 없다고 한다. 
+#하지만 v 이상을 입력받았을 때 v만큼만 처리하고싶었고, 이 점을 반영하여 10102-another.py를 작성했다.
 
 a=total.count("A")
 b=total.count("B")
