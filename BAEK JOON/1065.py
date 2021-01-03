e492b2156221265fc1c2ae26598b38006e432bd6
JI-YOUN-PARK <67935707+JI-YOUN-PARK@users.noreply.github.com>
2021-01-03 22:43:24 +0900

    2021-01-03

---
+++ BAEK JOON/1065.py
@@ -0,0 +1,10 @@
+N=int(input())
+if N<100:
+    t=N
+else:
+    t=99
+    for i in range(100,N+1):
+        n=list(map(int,str(i)))
+        if (n[0]-n[1]) == (n[1]-n[2]):
+            t = t+1
+print(t)
